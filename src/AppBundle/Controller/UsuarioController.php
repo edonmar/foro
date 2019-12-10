adc13263a3bbe1186686eebfd37ed5ee95bca902
edonmar <edonmar@gmail.com>
2019-12-10 22:38:25 +0100

    Añadido listado de usuarios

---
+++ src/AppBundle/Controller/UsuarioController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Repository\UsuarioRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsuarioController extends Controller
+{
+    /**
+     * @Route("/usuarios", name="usuario_listar")
+     */
+    public function indexAction(UsuarioRepository $usuarioRepository)
+    {
+        $usuarios = $usuarioRepository->findAll();
+
+        return $this->render('usuario/listar.html.twig', [
+            'usuarios' => $usuarios
+        ]);
+    }
+}
