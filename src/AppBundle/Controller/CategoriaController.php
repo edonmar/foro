6f93f38a294b594172413d42db1a6c91f346c434
esteban <edonmar@gmail.com>
2019-12-11 12:45:17 +0100

    Mostrar temas de una categoría seleccionada

--- src/AppBundle/Controller/CategoriaController.php
+++ src/AppBundle/Controller/CategoriaController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Categoria;
 use AppBundle\Repository\CategoriaRepository;
+use AppBundle\Repository\TemaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +19,19 @@ class CategoriaController extends Controller
 
         return $this->render('categoria/listar.html.twig', [
             'categorias' => $categorias
+        ]);
+    }
+
+    /**
+     * @Route("/categoria/temas/{id}", name="categoria_temas_listar")
+     */
+    public function temasAction(TemaRepository $temaRepository, Categoria $categoria)
+    {
+        $temas = $temaRepository->findByCategoria($categoria);
+
+        return $this->render('categoria/listar_temas.html.twig', [
+            'temas' => $temas,
+            'categoria' => $categoria
         ]);
     }
 }
---
+++ src/AppBundle/Repository/TemaRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use AppBundle\Entity\Tema;
+use AppBundle\Entity\Categoria;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+class TemaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Tema::class);
+    }
+
+    public function findByCategoria(Categoria $categoria)
+    {
+        return $this->createQueryBuilder('t')
+            ->select('t')
+            ->where('t.categoria = :categoria')
+            ->setParameter('categoria', $categoria)
+            ->orderBy('t.fijado', 'desc')
+            ->getQuery()
+            ->getResult();
+    }
+}
